a5d3f78131e6ef504da26c69eea23e3a9ef647ba
RafaelRosendof <rafael.19.rosendo@gmail.com>
2023-10-24 22:21:10 -0300

    vector and list

--- alg1.cpp
+++ alg1.cpp
@@ -16,13 +16,14 @@ using std::ostringstream;
 
 // alias = apelido
 using value_type = int;
-
+//fazendo uma linha em vim
 /// This function should return a string representation of the vector provided.
 /*!
  * @param A The array we need to convert its content to a string from.
  * @param len The length of the input array.
  * @return A string representation for the array's content.
  */
+
 template <size_t len>
 std::string to_string(const std::array<value_type, len> &A) {
     std::ostringstream oss;
@@ -36,26 +37,6 @@ std::string to_string(const std::array<value_type, len> &A) {
 
     return oss.str();
 }
-
-/*!
- * This function does a linear search in the array `A`, from index `l` to index
- * `r`, looking for the target `target`. Note that we are considering the closed
- * interval [l,r] over `A`.
- *
- * If target is found, the function return the index of the its first occurrence
- * in the array. Otherwise it returns the index just past the last element (i.e.
- * `r`+1).
- *
- * @param A The array where the search is done.
- * @param l The initial index that defines the searching space (range).
- * @param r The final index that defines the searching space (range).
- * @return The index of the first occurrence of `target`, or `r+1` otherwise.
- */
-
-// This is the client code.
-//implementar essa função postumo
-
-
 template<size_t n>
 size_t Bsearch(const std::array<value_type, n> &A, std::size_t l, size_t r, value_type target) {
     while (l <= r) {
@@ -105,4 +86,5 @@ int main() {
     return 0;
   }
 
-  
+
+
